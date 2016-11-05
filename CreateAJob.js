bc2263d5aeeb67088c473b73cf9cc8bce418f443
Alexander Leinoff <alexander-leinoff@uiowa.edu>
2016-11-05 17:43:09 -0500

    Very Stubbed out version of Creating a job
    Just does alerts for 2 kinds of jobs, need to put in the api calls now.

--- CreateAJob.js
+++ CreateAJob.js
@@ -13,14 +13,28 @@ import {
 } from 'react-native';
 
 import styles from './styles.js';
-
+import Button from 'react-native-button'
 
 class CreateAJob extends Component {
+
+  _makeAPlantingJob(){
+    AlertIOS.alert("trying to make planting job")
+  }
+
+  _makeAHarvestingJob(){
+    AlertIOS.alert("trying to make harvest job")
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}> This is where we will create new jobs</Text>
+        <Button style={styles.button} onPress={this._makeAPlantingJob.bind(this)}>
+          <Text> Make a Planting Job</Text>
+        </Button>
+        <Button style={styles.button} onPress={this._makeAHarvestingJob.bind(this)}>
+          <Text> MakeAHarvestingJob </Text>
+        </Button>
       </View>
     )
   }
